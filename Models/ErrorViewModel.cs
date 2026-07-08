ca0da2b88742d05fe8e6e46250c6977e3e6e9eaa
wa0102 <minon.maro@gmail.com>
2026-07-08 14:59:27 +0900

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace S.O.R.A._System_Operational_Remote_Assistant_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
